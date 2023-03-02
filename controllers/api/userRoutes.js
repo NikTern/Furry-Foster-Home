30206c474f2fdef9b46f3675dd1c2328d975ab54
Siyu Liu <siyu.java.web@gmail.com>
2023-03-02 22:41:28 +1100

    create user register and user login routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,60 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/auth.js'); 
-const { User,Pets } =require('../../models')
+const { User, Pets } = require('../../models');
+const bcrypt = require('bcrypt');
+
+router.post('/user/register', async (req, res) => { 
+    try {
+        const newUser = req.body;
+        const hasUser = await User.findOne(
+            {
+                where: { email: newUser.email }
+            }
+        );
+        if (hasUser) {
+            return res.json('email exist')
+        };
+        newUser.password = await bcrypt.hash(newUser.password, 10);
+        const userData = await User.create(newUser);
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) { 
+        res.status(400).json(err);
+    } 
+})
+
+router.post('/user/login', async (req, res) => { 
+    try {
+        const userData = await User.findOne({
+          where: { email: req.body.email },
+        });
+        if (!userData) {
+          res
+            .status(400)
+            .json({ message: "Incorrect email or password, please try again" });
+          return;
+        }
+
+        //TODO: need to add checkPassword() into UserProfile model
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+          res
+            .status(400)
+            .json({ message: "Incorrect email or password, please try again" });
+          return;
+        }
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+          res.json({ user: userData, message: "You are now logged in!" });
+        });
+      } catch (err) {
+        res.status(400).json(err);
+      }
+})
 
 router.get('/user/profile', withAuth,async (req, res) => { 
     try {
