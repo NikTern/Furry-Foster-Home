8da0136a59991e2c2aa7468afcbf9f362d093578
Sarah Mullock <smullock@hotmail.com>
2023-03-06 19:10:13 +1100

    add pet seed data and images

--- seeds/breed-seeds.js
+++ seeds/breed-seeds.js
@@ -33,6 +33,40 @@ const breedData = [
         breed_name: "Canary",
         category_id: 3,
     },
+    {
+        breed_name: "Indian Ringneck",
+        category_id: 3,
+    },
+    {
+        breed_name: "Lorikeet",
+        category_id: 3,
+    },
+    {
+        breed_name: "Pug",
+        category_id: 1,
+    },
+    {
+        breed_name: "Maltese Terrier",
+        category_id: 1,
+    },
+    {
+        breed_name: "German Shephard",
+        category_id: 1,
+    },
+    {
+        breed_name: "Cavoodle",
+        category_id: 1,
+    },
+
+    {
+        breed_name: "Domestic Short Hair",
+        category_id: 2,
+    },
+    {
+        breed_name: "Greyhound",
+        category_id: 1,
+    },
+
 ];
 
 const seedBreeds = () => Breed.bulkCreate(breedData);
