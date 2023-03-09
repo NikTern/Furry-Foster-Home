e2ac7d8723359fba2492aef155e38db63ac8c654
Siyu Liu <siyu.java.web@gmail.com>
2023-03-09 18:06:51 +1100

    fixed bug for running app from heroku

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,13 +3,17 @@ const router = require("express").Router();
 const { Pets, Category, Breed } = require("../models");
 
 
-
 router.get('/404-page', (req, res) => { 
   res.sendFile(path.join(__dirname, "../public/404.html"));
 })
 
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/html/login.html"));
+});
+
+router.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/html/signup.html"));
+});
+
 module.exports = router;
 
-router.get('/login', (req, res) => { 
-  res.sendFile(path.join(__dirname, "../public/html/login.html"));
-})
